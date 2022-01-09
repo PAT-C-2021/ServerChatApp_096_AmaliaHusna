f2e4550b017ac51af852e2c17d1445ba44f2804f
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2022-01-09 22:03:37 +0700

    Add project files.

---
+++ ServerChatApp_20190140096_Amalia Husna/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerChatApp_20190140096_Amalia_Husna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallBack));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready !!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class ServiceCallBack
+    {
+    }
+}
